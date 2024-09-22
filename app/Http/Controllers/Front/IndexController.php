1b58b3a48064ac199341d7d58747bd8330cbe143
ChinYeeLau <162902471+ChinYeeLau@users.noreply.github.com>
2024-09-22 16:45:05 +0800

    commit-95

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -16,7 +16,7 @@ class IndexController extends Controller
         $bestSellers=Product::where('is_bestseller','Yes')->where('status',1)->limit(8)->inRandomOrder()->with('section')->get();
         $discountedProducts=Product::where('product_discount','>',0)->where('status',1)->limit(8)->inRandomOrder()->with('section')->get();
         $featuredProducts=Product::where('is_featured','Yes')->where('status',1)->limit(8)->inRandomOrder()->with('section')->get();
-        $meta_title="E-Commerce Platform for Art Enthusiasts";
+        $meta_title="ArtNest-E-Commerce Platform for Art Enthusiasts";
         $meta_description="Online Shopping Website for Arts";
         $meta_keywords="eshop website,online shopping,art,prints,tshirt,hoodies,jewelry,bracelets";
         return view ('front.index')->with(compact('sliderBanners','fixBanners','newProducts','bestSellers','discountedProducts','featuredProducts','meta_title','meta_description','meta_keywords'));
